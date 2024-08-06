161a2bfe4365835e65ee808374f31e8e048e3c3e
Heather Strain <64221009+hestrain@users.noreply.github.com>
2024-08-05 20:14:58 -0400

    added all the associations. have most of the routes but some are not working

--- models/index.js
+++ models/index.js
@@ -1,32 +1,41 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
-})
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE',
-})
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-//i dont know how to use product tag here
-onDelete: 'CASCADE',
-})
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  //i dont know how to use product tag here
+  as: "tag_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-//i dont know how to use product tag here
-  onDelete: 'CASCADE',
-})
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  //i dont know how to use product tag here
+  as: "product_id",
+
+});
 
 module.exports = {
   Product,
